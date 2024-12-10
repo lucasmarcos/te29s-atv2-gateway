46b941ae5b467e8d8ba21d094238652f28aa435f
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-12-10 11:06:21 -0300

    servicos

---
+++ servico1/src/main/java/br/edu/utfpr/servico1/Servico1Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.servico1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servico1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servico1Application.class, args);
+	}
+
+}
---
+++ servico2/src/test/java/br/edu/utfpr/servico2/Servico2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.servico2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servico2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
